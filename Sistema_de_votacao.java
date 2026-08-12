0d35aa7e1f87642c2c1d35dbbe76be25f6827684
JoseFortunato-coder <jose.11881444@aluno.mg.gov.br>
2026-08-12 13:50:53 -0300

    Update Sistema_de_votacao.java

--- Sistema_de_votacao.java
+++ Sistema_de_votacao.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
-class Votacao {
+class SistemaVotacao {
 
     static int escolha_voto;
 
@@ -12,57 +12,74 @@ class Votacao {
         try {
             Scanner scanner = new Scanner(System.in);
             int escolha_quantia_candidatos;
-            System.out.print("===============================\n    CADASTRO DE CANDIDATOS    \n===============================\n\n");
-            System.out.print("Quantos candidatos deseja cadastrar? ");
+            int eleitores = 30;
+                System.out.print("===============================\n    CADASTRO DE CANDIDATOS    \n===============================\n\n");
+                System.out.print("Quantos candidatos deseja cadastrar? Só são permitidos 5. ");
 
-            escolha_quantia_candidatos = scanner.nextInt();
-            scanner.nextLine();
+                escolha_quantia_candidatos = scanner.nextInt();
+                scanner.nextLine();
+
+            if (escolha_quantia_candidatos < 6) {
+                List<String> candidatos = new ArrayList<>();
 
 
-            List<String> candidatos = new ArrayList<>();
+                for (int i = 1; i <= escolha_quantia_candidatos; i++) {
+                    System.out.print("Digite o nome do candidato " + i + ": ");
+                    String nome = scanner.nextLine();
+                    candidatos.add(nome);
+                }
 
-
-            for (int i = 1; i <= escolha_quantia_candidatos; i++) {
-                System.out.print("Digite o nome do candidato " + i + ": ");
-                String nome = scanner.nextLine();
-                candidatos.add(nome);
-            }
-
-            System.out.println("\n=== CANDIDATOS REGISTRADOS ===");
-            for (int i = 0; i < candidatos.size(); i++) {
-                System.out.print("\n");
-                System.out.print("Nome: " + candidatos.get(i));
-            }
+                System.out.println("\n=== CANDIDATOS REGISTRADOS ===");
+                for (int i = 0; i < candidatos.size(); i++) {
+                    System.out.print("\n");
+                    System.out.print("Nome: " + candidatos.get(i));
+                }
 
 //---------------------------------------------------------------------------------------------------------------
-
-            System.out.println("\n\n=== INICIAR VOTAÇÃO ===");
-            for (int i = 0; i < candidatos.size(); i++) {
-                System.out.println("\nCandidato: " + i);
-                System.out.print("Nome: " + candidatos.get(i));
-            }
-
-
-            System.out.println("\n\nDigite 0 para sair e 1 para continuar a votação");
-            double continuar = scanner.nextDouble();
-            scanner.nextLine();
-
-            if (continuar == 1) {
+                int[] votos = new int[eleitores];
+                System.out.println("\n\n=== INICIAR VOTAÇÃO ===");
                 for (int i = 0; i < candidatos.size(); i++) {
-                    System.out.println("\nFaça o seu voto de 0 a " + escolha_quantia_candidatos + ":");
-                    escolha_voto = Integer.parseInt(scanner.nextLine());
-
+                    System.out.println("\nCandidato: " + i);
+                    System.out.print("Nome: " + candidatos.get(i));
                 }
-            } else {
-                System.out.println("Saindo...");
-            }
+
+
+                System.out.println("\n\nDigite 0 para sair e 1 para continuar a votação");
+                double continuar = scanner.nextDouble();
+                scanner.nextLine();
+
+                if (continuar == 1) {
+                    for (int i = 0; i < candidatos.size(); i++) {
+                        int diferenca = votos[i] - votos[i + 1];
+                    }
+//-------------------------------------------------------------------------------------------------------
+                    int maiorQtdVotos = -1;
+                    String nomeVencedor = "";
+
+                    for (int i = 0; i < candidatos.size(); i++) {
+                        int votosDoCandidato = votos[i];
+
+                        if (votosDoCandidato > maiorQtdVotos) {
+                            maiorQtdVotos = votosDoCandidato;
+                            nomeVencedor = candidatos.get(i);
+                        }
+                    }
+
+                    System.out.println("\nO candidato vencedor foi: " + nomeVencedor);
+
+                } else {
+                    System.out.println("Saindo...");
+                }
 
                 scanner.close();
+            } else if (escolha_quantia_candidatos > 5) {
+                System.out.println("Só são permitidos 5 candidatos");
+            }
 
+        }catch(Exception e){
+                System.out.println("Faça uma digitação valida, sem números quando se usa só texto.");
 
-        }catch (Exception e) {
-            System.out.println("Faça uma digitação valida, sem números quando se usa só texto.");
+            }
 
-        }
     }
 }
