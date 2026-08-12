1a0a7ec47dcb50a075fcc4e215f871d6ad993253
MatsHubTch <mateus.11881423@aluno.mg.gov.br>
2026-08-12 09:39:41 -0300

    Update Sistema_de_votacao.java

--- Sistema_de_votacao.java
+++ Sistema_de_votacao.java
@@ -1,19 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 class Votacao {
-
     public static void main(String[] args) {
-        int escolha;
-
-        System.out.print("===============================\n  SISTEMA DE VOTAÇÃO ESCOLAR    \n===============================\n\n");
-
-        System.out.print("1 - Cadastrar candidadtos\n2 - Iniciar votação");
-        System.out.print("3 - Exibir resultado\n4 - Exibir matriz de votos\n5 - Encerrar sistema\n\n");
-        System.out.print("Escolha uma opção:");
-
-        int escolha = teclado.nextInt();
+        Scanner scanner = new Scanner(System.in);
 
 
+        System.out.print("===============================\n    CADASTRO DE CANDIDATOS    \n===============================\n\n");
+
+        System.out.print("Quantos candidatos deseja cadastrar?");
+        int escolha_quantia_candidatos = scanner.nextInt();
+
+        for (int i = 1; i <= escolha_quantia_candidatos; i++) {
+            System.out.println("\nCandidato");
 
 
+            scanner.close();
+        }
     }
 }
